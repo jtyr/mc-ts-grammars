6ea8a3a96663c9333fb32e28b863d98ab46cd96f
jtyr <3788909+jtyr@users.noreply.github.com>
2026-09-05 10:19:34 +0000

    Update grammars: html_blade foam lean swift mlir proto twig kcl ssh_client_config caddy javadoc fluentbit ocaml

--- grammars/proto/src/queries/indents.scm
+++ grammars/proto/src/queries/indents.scm
@@ -3,6 +3,7 @@
   (enum_body)
   (oneof)
   (service)
+  (block_lit)
 ] @indent.begin
 
 ; rpc's body is optional (it can end in ";" instead of "{ }"), so anchor on
@@ -12,6 +13,11 @@
   "{" @indent.begin)
 
 "}" @indent.end @indent.branch
+
+; A block_lit may be delimited by angle brackets instead of braces. Anchor on
+; the node so this doesn't fire for the ">" that closes a map<K, V> type.
+(block_lit
+  ">" @indent.end @indent.branch)
 
 [
   (ERROR)
